42a0a1064072694f3ceceedde882f093cd36db74
Fernando Batista <wiesecredito@gmail.com>
2021-10-25 11:55:48 -0300

    Primeiro commit de cadastro do usuário

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,33 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    var nome = " "
+    var idade = 0
+    var emprego = " "
+    var nacionalidade = " "
+    var tipoSanguineo = " "
+    var nascimento: Int
+    var leitura = Scanner(System.`in`)
+
+
+    print("Digite o seu nome de usuário: ")
+    nome = leitura.next()
+
+    print("Informe a sua idade: ")
+    idade = leitura.nextInt()
+    //Informando a idade acima e o cálculo para saber o ano de nascimento, logo abaixo.
+    nascimento = 2021 - idade
+
+    print("Qual a sua preferência de emprego? ")
+    emprego = leitura.next()
+
+    print("Qual a sua nacionalidade? ")
+    nacionalidade = leitura.next()
+
+    print("Qual o seu tipo sanguíneo? ")
+    tipoSanguineo = leitura.next()
+
+    println("O seu nome é $nome, você é do(a) $nacionalidade, e nasceu no ano de $nascimento. Você tem pretensão de trabalhar" +
+            " como $emprego, e seu tipo sanguíneo é $tipoSanguineo.")
+}
